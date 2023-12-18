cc85499789ccca9fdda15c90433da21c4a72dbde
zvis70 <tzviks.s@gmail.com>
2023-12-18 18:47:22 +0200

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "zvis70/mister-mail.git",
-  plugins: [react()],
+    plugins: [react()],
 })
